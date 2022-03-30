7b9bbdcbbe753ddf40dbb26eab3e12a2a6545ea9
Ohadklein11 <ohadklein11@gmail.com>
2022-03-30 22:53:54 +0300

    Update city_temperature_prediction.py

--- exercises/city_temperature_prediction.py
+++ exercises/city_temperature_prediction.py
@@ -4,8 +4,10 @@ from IMLearn.utils import split_train_test
 
 import numpy as np
 import pandas as pd
+import plotly.graph_objects as go
 import plotly.express as px
 import plotly.io as pio
+
 pio.templates.default = "simple_white"
 
 
@@ -41,7 +43,44 @@ if __name__ == '__main__':
     data = load_data('..\\datasets\\City_Temperature.csv')
 
     # Question 2 - Exploring data for specific country
-    raise NotImplementedError()
+
+    israeli_data = data[data['Country'] == 'Israel']
+    scatters = []
+    monthly_temps = []
+    for _ in range(12):
+        monthly_temps.append([])
+    for year in israeli_data['Year'].unique():
+        temps_per_day = []
+        for day in range(1, 366):
+            temp_row = israeli_data[(israeli_data['Year'] == year) &
+                                    (israeli_data['DayOfYear'] == day)]
+            if temp_row.Temp.size == 1:
+                temp = temp_row.Temp.values[0]
+                temps_per_day.append((day, temp))
+                month_idx = temp_row.Month.values[0] - 1
+                monthly_temps[month_idx].append(temp)
+        temps_per_day = pd.DataFrame(temps_per_day)
+        scatters.append(go.Scatter(x=temps_per_day[0],
+                                   y=temps_per_day[1],
+                                   mode='markers', name=f'{year}'))
+    fig = go.Figure(data=scatters)
+    fig.update_layout(
+        title="(Q3.2.2) Temperature in Israel by day of year",
+        xaxis_title="Day of Year",
+        yaxis_title="Temperature")
+    # fig.show()
+    # it seems like a polynomial of degree 3 might be fitting the graph
+
+    # plot the standard deviation of the daily temperatures for each month
+
+    monthly_devs = [np.std(temps) for temps in monthly_temps]
+    fig = go.Figure(data=[go.Bar(x=np.linspace(1, 12, 12).astype(int),
+                                 y=monthly_devs)])
+    fig.update_layout(
+        title="(Q3.2.2) Monthly deviation of temperatures",
+        xaxis_title="Month",
+        yaxis_title="Temperature Deviation")
+    fig.show()
 
     # Question 3 - Exploring differences between countries
     raise NotImplementedError()
